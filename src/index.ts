ca374ed644130e676d973266ed4293b575541857
Bilalusuf93 <42065447+Bilalusuf93@users.noreply.github.com>
2023-03-05 15:35:15 +0500

    classes in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-function caculateTax(income: number): number {
-    if (income < 50_000) {
-        return 1.2
+class Account {
+    // parameter property.
+    constructor(public readonly id: number, public owner: string, private _balance: number, public nickName?: string) {
+        // this.id = id;
+        // this.owner = owner;
+        // this._balance = balance;
     }
-    return 1.5
+
+    private calculateTax() {
+
+    }
+
+    deposit(amount: number) {
+        if (amount <= 0) {
+            throw new Error("Invalid amount");
+        }
+        else {
+            this.calculateTax();
+            this._balance +=amount;
+        }
+    }
+
+    get balance() {
+        return this._balance;
+    }
 }
+
+let account = new Account(1, 'Bilal', 50);
+// account.id = 0;
+account.deposit(50);
+console.log(account.balance);
