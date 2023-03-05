21418fe7c10c05024676d814baa0b3bab09f5eeb
Bilalusuf93 <42065447+Bilalusuf93@users.noreply.github.com>
2023-03-05 15:38:43 +0500

    Index signature property

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,8 @@
-class Account {
-    // parameter property.
-    constructor(public readonly id: number, public owner: string, private _balance: number, public nickName?: string) {
-        // this.id = id;
-        // this.owner = owner;
-        // this._balance = balance;
-    }
-
-    private calculateTax() {
-
-    }
-
-    deposit(amount: number) {
-        if (amount <= 0) {
-            throw new Error("Invalid amount");
-        }
-        else {
-            this.calculateTax();
-            this._balance +=amount;
-        }
-    }
-
-    get balance() {
-        return this._balance;
-    }
+class SeatAssignment {
+    // index signature property
+    [seatNumber: string]: string;
 }
 
-let account = new Account(1, 'Bilal', 50);
-// account.id = 0;
-account.deposit(50);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = 'Bilal';
+seats['A2'] = 'usma'
